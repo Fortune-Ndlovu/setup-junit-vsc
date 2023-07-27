34528ab52ead9cb07ab2737b5c373039ac71f69f
Fortune Ndlovu <ndlovufortune97@gmail.com>
2023-07-27 16:48:02 +0100

    Initial Commit

---
+++ src/test/java/com/example/CodeTest.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CodeTest {
+
+    @Test
+    public void testSayHello() {
+        Code code = new Code();
+        assertEquals("Hello world! its Fortune", code.sayHello());
+    }
+}
